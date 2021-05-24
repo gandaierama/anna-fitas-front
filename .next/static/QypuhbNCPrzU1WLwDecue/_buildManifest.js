ff1bb70d5a4c244d930b1eb36b7568302236349c
Daniel BG <daniel@bettrads.com>
2021-05-24 01:42:04 -0300

    ft/next

---
+++ .next/static/QypuhbNCPrzU1WLwDecue/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-989f83c1703e0db82b85.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ pages/sobre.js
@@ -0,0 +1,5 @@
+function About() {
+  return <div>Sobre</div>
+}
+
+export default Sobre
